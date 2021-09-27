944e1f32f3bb4c41beec3725e96bd69042c0d2ef
Adam <adam@thinkaliker.com>
2021-09-26 20:33:20 -0700

    helloworld test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import grpc from 'grpc-web';
+// import protoLoader from '@grpc/proto-loader';
+import { HelloRequest } from '@okmeme/shared';
+import { GreeterClient } from '@okmeme/shared';
+
+
+// const PROTO_PATH = __dirname + '/../node_modules/@okmeme/shared/protos/helloworld.proto';
+// const packageDefinition = protoLoader.loadSync(
+//   PROTO_PATH,
+//   {
+//     keepCase: true,
+//     longs: String,
+//     enums: String,
+//     defaults: true,
+//     oneofs: true
+//   });
+// const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+const request = new HelloRequest();
+const target = 'http://localhost:9999';
+// const target = 'http://localhost:50051'; 
+const user = 'asdf';
+request.setName(user);
+var res = '';
+const client = new GreeterClient(target);
+console.log(client)
+console.log(request)
+client.sayHello(request, {}, (err, response) => {
+  console.log(err)
+  console.log(response);
+  res = response.getMessage();
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{res}</p>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {sayHello} from "@okmeme/shared";
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -13,7 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-sayHello();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
